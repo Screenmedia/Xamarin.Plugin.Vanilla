63f50b2514623a8d26848b416da195e59fca3396
Tom Curran <tom.curran@screenmedia.co.uk>
2017-07-03 22:43:04 +0100

    migrated source solution to netstandard

--- src/Screenmedia.Plugin.Vanilla.Android/Properties/AssemblyInfo.cs
+++ src/Screenmedia.Plugin.Vanilla.Android/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Screenmedia.Plugin.Vanilla.Droid")]
+[assembly: AssemblyTitle("Screenmedia.Plugin.Vanilla.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("b099l3")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ src/Screenmedia.Plugin.Vanilla.Shared/CrossIceCreamMachine.cs
@@ -0,0 +1,43 @@
+﻿﻿﻿using System;
+using Screenmedia.Plugin.Vanilla.Abstractions;
+
+namespace Screenmedia.Plugin.Vanilla
+{
+    /// <summary>
+    /// Cross platform IceCreamMachine implemenations
+    /// </summary>
+    public static class CrossIceCreamMachine
+    {
+        static Lazy<IIceCreamMachine> Implementation = new Lazy<IIceCreamMachine>(CreateIceCreamMachine, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static IIceCreamMachine Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IIceCreamMachine CreateIceCreamMachine()
+        {
+#if NETSTANDARD1_0
+            return null;
+#else
+            return new IceCreamMachineImplementation();
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
+    }
+}
